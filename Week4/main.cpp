0e5c4cb839dcecc3adf1e00076e3c5437283523b
Bob <bobjeltes@hotmail.com>
2018-10-20 12:06:19 +0200

    Week 4 halfway

--- Week4/main.cpp
+++ Week4/main.cpp
@@ -4,27 +4,59 @@
 int main() {
     //std::cout << "Hello, World!" << std::endl;
 
-    //char in_stream;
+    //char in_stream1;
 
-    std::ifstream in_stream;
+    std::ifstream in_stream1;
+    std::ifstream in_stream2;
+    std::ofstream out_streamReverse;
+    std::ofstream out_streamCombine;
 
-    in_stream.open("C:/Users/Bob/Documents/GitHub/OBOPB/Les4/jabberwocky.txt");
+    in_stream1.open("C:/Users/Bob/Documents/GitHub/OOP-Basic/Week4/jabberwocky.txt");
+    in_stream2.open("C:/Users/Bob/Documents/GitHub/OOP-Basic/Week4/jabberwocky_lat.txt");
+    out_streamCombine.open("C:/Users/Bob/Documents/GitHub/OOP-Basic/Week4/jabberwocky_combine.txt");
+    out_streamReverse.open("C:/Users/Bob/Documents/GitHub/OOP-Basic/Week4/ykcowrebbaj.txt");
 
-    if (!in_stream) {
+    if (!in_stream1) {
         std::cout << "Probleem bij openen file" << std::endl;
         exit(1);
     }
-
-    char ch;
-    in_stream.get(ch);
-    std::cout << ch << std::endl;
-
-    while (!in_stream.eof()) {
-        in_stream.get(ch);
-        std::cout << ch;
+    if (!out_streamReverse) {
+        std::cout << "Probleem bij openen file" << std::endl;
+        exit(2);
     }
 
-    in_stream.close();
+    char ch1;
+    char ch2;
+    int docLength = 0;
+
+    /*
+    in_stream1.get(ch);
+    std::cout << ch << std::endl;
+    */
+
+    while (!in_stream1.eof()) {
+        in_stream1.get(ch1);
+        in_stream2.get(ch2);
+        std::cout << ch1;
+        docLength++;
+        out_streamCombine.put(ch1);
+        out_streamCombine.put(ch2);
+    }
+
+    /*
+    while (docLength > 0) {
+        in_stream1.get(ch1);
+        docLength--;
+    }
+     */
+
+
+    std::cout << "Doc length: " << docLength << " characters" << std::endl;
+
+
+    in_stream1.close();
+    out_streamReverse.close();
+
 
     return 0;
 }
