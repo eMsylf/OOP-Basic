ba73704a6b55e5c3617f9bcc0d1faec8eb44292f
Bob <bobjeltes@hotmail.com>
2019-02-21 11:44:29 +0100

    Main experiment
    Experiment om te proberen alle functionaliteit in de basis werkend te krijgen door uitsluitend in de main te werken. Inheritance is moeilijk

--- Week3MainExperiment/main.cpp
+++ Week3MainExperiment/main.cpp
@@ -2,25 +2,53 @@
 #include <string>
 
 class Munt {
-private:
-    std::string name;
-    virtual float value = 0;
 public:
-    std::string GetName() {
-        return name;
-    }
+    std::string name;
+    float value;
+
+
     float GetValue() {
         return value;
+    }
+
+
+    std::string GetName() {
+        return name;
     }
 };
 
 class Vijftig_Cent : public Munt {
 private:
     std::string name = "vijftig cent muntje";
-    float value = 0.50;
+    //float value = 0.50;
+
+public:
+    //const char* Name;
+};
+
+class EenEuro : public Munt {
+private:
+
+
+public:
+    std::string name = "een euro muntje";
+    /*std::string GetName () {
+        return name;
+    }*/
+
+
 };
 
 int main() {
+    EenEuro eenEuro;
+
+    std::cout << eenEuro.GetName() << std::endl; // deze shit wil de naam die ik bij EenEuro heb ingevoerd niet printen
+
+
+    //std::cout << sizeof(Munt) << std::endl;
+    //std::cout << sizeof(Vijftig_Cent) << std::endl;
+
+    /*
     std::cout << "Hallo, dit is een test om te kijken of ik het hele koffieapparaat in de main kan modelleren" << std::endl;
 
     std::cout << "---------" << std::endl;
@@ -32,6 +60,6 @@ int main() {
 
     std::cout << "Munt naam: " << muntje.GetName() << std::endl;
     std::cout << "Munt waarde: \u20AC" << muntje.GetValue() << std::endl;
-
+    */
     return 0;
 }
